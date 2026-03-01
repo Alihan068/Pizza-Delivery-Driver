02d7dc61fbce010171d97473af07088a45ba2ab3
Alihan <alihankomurcuoglu@gmail.com>
2026-03-02 01:03:18 +0300

    ExtractionZone

---
+++ Assets/Scripts/ExtractionZone.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ExtractionZone : MonoBehaviour {
+    [Header("Settings")]
+    [SerializeField] KeyCode interactKey = KeyCode.E;
+    [SerializeField] GameObject visualIndicator;
+
+    ScoreHandler scoreHandler;
+
+    private void Start() {
+        scoreHandler = FindFirstObjectByType<ScoreHandler>();
+        if (visualIndicator != null) visualIndicator.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+
+            if (visualIndicator != null) visualIndicator.SetActive(true);
+
+            if (Input.GetKey(interactKey)) {
+                scoreHandler.EndLevel(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            if (visualIndicator != null) visualIndicator.SetActive(false);
+        }
+    }
+}
